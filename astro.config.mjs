a3eecd9e92e3b08121dc4dfee6359829d6db39c3
pot <954693353gyz@gmail.com>
2025-11-14 18:18:27 +0800

    feat: 处理blog数据结构

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,35 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
+// 博客内容分类枚举
+const CATEGORIES = z.enum([
+	'Tech', // 技术
+	'Life', // 生活
+	'Movie', // 电影
+	'Music', // 音乐
+]);
+
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
+			// 为了优化主页展示，生活随笔要加短标题
+			shortTitle: z.string().optional(),
 			description: z.string(),
-			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
+			// 文章头图
 			heroImage: image().optional(),
+			// 展示图
+			coverImage: image().optional(),
+			// 草稿过滤
 			draft: z.boolean().optional(),
+			// 分类
+			category: CATEGORIES,
+			// 标签
+			// 可能会用上，再分类
+			tags: z.array(z.string()).optional(),
 		}),
 });
 
