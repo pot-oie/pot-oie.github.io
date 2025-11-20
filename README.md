7ecfe29225893385e4b93578814c3914447ba3b5
pot <954693353gyz@gmail.com>
2025-11-20 23:56:43 +0800

    feat: FULL UPDATE

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 ## TODO
 
-- scroll 视图调整
-- moviecard hover 调整
-- 日历视觉调整
 - about page 调整
-- 明暗切换实现
--- src/data/music.ts
+++ src/data/music.ts
@@ -1,6 +1,7 @@
 import type { ImageMetadata } from 'astro';
 import coverQiude from '../assets/music/qiude-4.jpg';
 import coverKiriT from '../assets/music/kiriTsome.webp';
+import coverRaye from '../assets/music/Genesis.png';
 
 export interface MusicData {
   title: string;
@@ -16,13 +17,24 @@ export interface MusicData {
 
 export const allMusic: MusicData[] = [
   {
+    title: 'Genesis',
+    artist: 'RAYE',
+    coverImage: coverRaye,
+    pubDate: new Date('2025-11-20'),
+    links: {
+      spotify: null,
+      netease: null,
+      qqMusic: 'https://c6.y.qq.com/base/fcgi-bin/u?__=t6Z0nVT35Vep',
+    }
+  },
+  {
     title: '有些话要用英文说',
     artist: 'Kiri T',
     coverImage: coverKiriT,
     pubDate: new Date('2025-11-17'),
     links: {
       spotify: null,
-      netease: 'https://music.163.com/song?id=2162950892&uct2=U2FsdGVkX19Cjto4L6m1CIDNLVuZKeiWLWcuFvWkmps=', // 示例链接
+      netease: 'https://music.163.com/song?id=2162950892&uct2=U2FsdGVkX19Cjto4L6m1CIDNLVuZKeiWLWcuFvWkmps=',
       qqMusic: 'https://c6.y.qq.com/base/fcgi-bin/u?__=GYANphmIs4EP',
     }
   },
@@ -33,7 +45,7 @@ export const allMusic: MusicData[] = [
     pubDate: new Date('2025-11-15'),
     links: {
       spotify: null,
-      netease: 'https://music.163.com/song?id=2750712095&uct2=U2FsdGVkX1+OipRIW+oTz8hmNAAB1Q57Uj+CvubhR8w=', // 示例链接
+      netease: 'https://music.163.com/song?id=2750712095&uct2=U2FsdGVkX1+OipRIW+oTz8hmNAAB1Q57Uj+CvubhR8w=',
       qqMusic: null,
     }
   },
