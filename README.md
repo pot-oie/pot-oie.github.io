c740e58d4422e59345892ad90005cdacd612fbef
pot <954693353gyz@gmail.com>
2025-11-20 09:32:46 +0800

    fix: blogpost修复

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO
 
-- 字号调整
 - scroll 视图调整
 - moviecard hover 调整
 - 日历视觉调整
 - about page 调整
+- 明暗切换实现
--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
 
   markdown: {
     shikiConfig: {
-      theme: 'dark-plus',
+      theme: 'github-light',
     },
   },
 
